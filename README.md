f58df88661ed8322556843a61ac100a91416c3e1
Emilia-Cal <emilia.cal@proton.me>
2025-12-08 08:21:23 +0100

    Fill out README and create Pseudocode for project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-rock-paper-scissors
+
+This projects aim is to demonstrate basic javascript capabilities by setting up a working,
+Rock Paper Scissor Bot to play against. Ultimately the User should be able to play 5 Rounds against the bot,
+and in the End a winner is determined based on points achieved during the Rounds.
--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+// Create Function that allows the computer to randomly pick either Rock Paper or Scissor
+function getComputerChoice(){
+
+};
+// Create Function that allows the player to input Rock Paper or Scissor, the input should be case insensitive.
+
+// Create Function that compares both Inputs with one another and determin a winner, a loser, or a tie and announces the result in the console.
+
+// Create Function that plays 5 Rounds and keeps track of the Score of players, once 5 rounds are played a winner is determined.
+// Output the total winner to the console log.
