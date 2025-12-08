e6e88c3e391791079742dea630a5075fdd3b565a
Emilia-Cal <emilia.cal@proton.me>
2025-12-08 09:01:29 +0100

    Add Logic for Human Player to pick one of 3 options

--- javascript.js
+++ javascript.js
@@ -17,6 +17,15 @@ function getComputerChoice(){
 console.log(getComputerChoice());
 // Create Function that allows the player to input Rock Paper or Scissor, the input should be case insensitive.
 
+function getHumanChoice(){
+    let humanDecision = prompt("Please Enter 'Rock', 'Paper', or 'Scissor'")
+    function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    };
+    return capitalize(humanDecision);
+};
+
+console.log(getHumanChoice())
 // Create Function that compares both Inputs with one another and determin a winner, a loser, or a tie and announces the result in the console.
 
 // Create Function that plays 5 Rounds and keeps track of the Score of players, once 5 rounds are played a winner is determined.
