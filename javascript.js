81986f9878dd284a7b65fdf28bf15e266c4c2eac
Emilia-Cal <emilia.cal@proton.me>
2025-12-09 19:46:51 +0100

    Finish the Game creation
    Build the logic for the individual rounds as well as a s score counter
    that plays until a player reaches 5 points.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,5 @@
 // Create Function that allows the computer to randomly pick either Rock Paper or Scissor
-let humanScore = 0;
-let computerScore = 0;
-
-function getComputerChoice(){
+function getComputerInput(){
     const minCeiled = Math.ceil(1);
     const maxFloored = Math.floor(4);
     let computerNmbr = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
@@ -18,7 +15,7 @@ function getComputerChoice(){
 };
 // Create Function that allows the player to input Rock Paper or Scissor, the input should be case insensitive.
 
-function getHumanChoice(){
+function getHumanInput(){
     let humanDecision = prompt("Please Enter 'Rock', 'Paper', or 'Scissor'")
     function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
@@ -26,19 +23,60 @@ function getHumanChoice(){
     return capitalize(humanDecision);
 };
 // Create Function that compares both Inputs with one another and determin a winner, a loser, or a tie and announces the result in the console.
-let humanInput = getHumanChoice();
-let computerInput = getComputerChoice();
-
-function playRound(humanChoice, computerChoice){
-    if (humanChoice == "Rock" && computerChoice == "Rock" || humanChoice == "Paper" && computerChoice == "Paper" || humanChoice == "Scissor" && computerChoice == "Scissor") {
-        alert("It's a Tie!")
-    }
-    else {
-        console.log("It's not a tie.")
-    };
-    return;
-};
-
-playRound(humanInput, computerInput);
+let humanInput;
+let computerInput;
+let humanScore = 0;
+let computerScore = 0;
 // Create Function that plays 5 Rounds and keeps track of the Score of players, once 5 rounds are played a winner is determined.
+function bestOf5(humanPoints, computerPoints) {
+    if (humanPoints < 5 && computerPoints < 5) {
+        playRound();
+        function playRound(){
+            humanInput = getHumanInput();
+            computerInput = getComputerInput();
+            if (humanInput === computerInput) {
+                alert("It's a Tie!")
+                humanScore++; 
+                computerScore ++;
+            } else if (humanInput == "Rock" && computerInput == "Scissor") {
+                alert("Rock beats Scissor! :)")
+                humanScore++;
+            } else if (humanInput == "Rock" && computerInput == "Paper") {
+                alert("Paper beats Rock! :(")
+                computerScore++;
+            } else if (humanInput == "Paper" && computerInput == "Rock") {
+                alert("Paper beats Rock! :)")
+                humanScore++;
+            } else if (humanInput == "Paper" && computerInput == "Scissor") {
+                alert("Scissor beats Paper! :(")
+                computerScore++;
+            } else if (humanInput == "Scissor" && computerInput == "Paper") {
+                alert("Scissor beats Paper! :)")
+                humanScore++;
+            } else if (humanInput == "Scissor" && computerInput == "Rock") {
+                alert("Rock beats Scissor! :(")
+                computerScore++;
+            } else {
+                alert("Something went wrong!!!!11!!");
+            }
+            return;
+        };    
+        console.log("Player 1 has " + humanScore + " Points" + "\n" + "Player 2 has " + computerScore + " Points")
+        return bestOf5(humanScore, computerScore);
+    } else {
+        announceWinner(humanScore, computerScore);
+    }
+}
 // Output the total winner to the console log.
+function announceWinner(humanPoints, computerPoints) {
+    if (humanPoints > computerPoints) {
+        (alert("Player 1 Won the Game!!! Yaaay!"))
+    } else if (humanPoints < computerPoints) {
+        alert("Player 2 Won the Game!!! Congrats!!")
+    } else if (humanPoints === computerPoints) {
+        alert("Both of you tied 5 times! WOW!!")
+    } else {
+        alert("Something went wrong :o")
+    }
+    return alert("The game is over, refresh the Page to play another game!")
+}
